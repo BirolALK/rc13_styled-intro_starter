3e092829e04ce268007b6a98aea9cc86dd039bdd
Birol ALKAN <birolalkan93@gmail.com>
2022-11-02 11:10:03 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Button, TomatoButton } from "./components/Button.styled";
+
+
+const App = () => {
+  return (
+    <>
+      <Button primary>Save</Button>
+      <Button>Clear</Button>
+      <TomatoButton>Send</TomatoButton>
+      <TomatoButton primary>Submit</TomatoButton>
+    </>
+  );
+};
+
+export default App;
